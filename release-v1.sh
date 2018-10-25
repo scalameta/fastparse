1f59e8f42f0d9ae26e33fa706a899544cc9eba2b
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-10-25 16:23:04 +0200

    Add release script

---
+++ release-v1.sh
@@ -0,0 +1,4 @@
+
+sbt ++2.11.11 utilsJVM/publishSigned utilsJS/publishSigned utilsNative/publishSigned fastparseJVM/publishSigned fastparseJS/publishSigned fastparseNative/publishSigned
+sbt ++2.12.3  utilsJVM/publishSigned utilsJS/publishSigned                           fastparseJVM/publishSigned fastparseJS/publishSigned fastparseNative/publishSigned
+sbt sonatypeReleaseAll
