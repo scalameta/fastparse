6bc00d41bfb4edad24cc6d9fcfe3f125980ff0ff
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-04-08 18:01:52 +0200

    Bump scala version

--- project/Constants.scala
+++ project/Constants.scala
@@ -1,6 +1,6 @@
 package scala.meta.internal.fastparse
 object Constants{
-  val version = "1.0.0"
+  val version = "1.0.1"
   val scala210 = "2.10.7"
   val scala211 = "2.11.12"
   val scala212 = "2.12.8"
