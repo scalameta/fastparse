ef3ba2ebe4071d68a918af429cf5656aee22c786
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-04-08 18:01:21 +0200

    Add publish script

---
+++ bin/publishSigned.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+sbt ++2.10.7 \
+    fastparseJS/publishSigned \
+    fastparseJVM/publishSigned
+sbt ++2.11.12 \
+    fastparseJS/publishSigned \
+    fastparseJVM/publishSigned \
+    fastparseNative/publishSigned
+sbt ++2.12.8 \
+    fastparseJS/publishSigned \
+    fastparseJVM/publishSigned
+sbt ++2.13.0-RC1 \
+    fastparseJS/publishSigned \
+    fastparseJVM/publishSigned
